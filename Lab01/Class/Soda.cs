e2ddca902abf9d8e0fbeeada0f2451af10ccfdc8
Mario Jose Roldan <mariojudoka@gmail.com>
2020-01-27 14:58:24 -0600

    Hellow Word
    Stard the proyect, and create the class too soda

---
+++ Lab01/Class/Soda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab01.Class
+{
+    public class Soda
+    {
+        public string Name { get; set; } //id 
+        public string Flavor { get; set; }
+        public int Volum { get; set; }
+        public int Price { get; set; }
+        public string Made { get; set; }//its the house of create the flavor and made the final product 
+    }
+}
