883abdf1be0c55600c7ae530255da43651483d02
waloro <walterof1@hotmail.com>
2020-02-03 15:20:18 -0600

    commit 3/2/20
    complet

--- Lab01/Class/Soda.cs
+++ Lab01/Class/Soda.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Lab01.Class
+﻿namespace Lab01.Class
 {
     public class Soda
     {
         public string Name { get; set; } //id 
-        public string Flavor { get; set; } 
+        public string Flavor { get; set; }
         public int Volum { get; set; }
         public double Price { get; set; }
         public string Made { get; set; }//its the house of create the flavor and made the final product 
